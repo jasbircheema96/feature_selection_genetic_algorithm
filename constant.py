cfd653d80c0c8852068b5656312266b4c5cc9321
JASBIR SINGH <jasbircheema96@gmail.com>
2019-06-07 20:26:52 -0700

    first commit

---
+++ constant.py
@@ -0,0 +1,16 @@
+# path to training data file
+train_data_path = 'C:\\Users\\jasbi\\Downloads\\isolet1+2+3+4.data'
+# path to test data file
+test_data_path = 'C:\\Users\\jasbi\\Downloads\\isolet5.data'
+# number of tress in random forest
+num_estimators_RF=100
+# total number of extracted features
+D=167
+# size of feature subset (according to research paper)
+d=15
+# population size (represented by mew in research paper)
+mew=1000
+# number of individuals selected from any generation for mating
+pooling_mate_size=100
+# mutation probability
+mutation_prob=0.1
