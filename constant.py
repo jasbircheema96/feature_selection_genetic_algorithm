e2c7cdb12116471b306fff84b1137d8d56116ca7
JASBIR SINGH <jasbircheema96@gmail.com>
2019-06-13 15:41:32 -0700

    data files added

--- constant.py
+++ constant.py
@@ -1,15 +1,13 @@
 # path to training data file
-train_data_path = 'C:\\Users\\jasbi\\Downloads\\isolet1+2+3+4.data'
+train_data_path = 'isolet1+2+3+4.data'
 # path to test data file
-test_data_path = 'C:\\Users\\jasbi\\Downloads\\isolet5.data'
+test_data_path = 'isolet5.data'
 # total number of extracted features
 D=617
 # size of feature subset (according to research paper)
 d=20
 # population size (represented by mew in research paper)
-mew=10
-# number of individuals selected from any generation for mating
-#mating_pool_size=5
+mew=20
 # number of individuals to retain from previous generation
 retain_previous=2
 # number of genes changes per mutation
