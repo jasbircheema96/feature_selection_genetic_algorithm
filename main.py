c50f0a7ae57a8bc82f743eeda07d406055638628
JASBIR SINGH <jasbircheema96@gmail.com>
2019-08-11 08:30:59 +0530

    time computation added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import constant
+import time
 from generation import next_generation
 from generation import best_individual
 from generation import average_fitness
@@ -8,7 +9,10 @@ def main():
     average_file = open("average.txt", "a")
     best_file=open("best.txt","a")
     features_file=open("features.txt","a")
+    time_file=open("time.txt","a")
+    start_time=time.time()
     for i in range(constant.num_iterations):
+        # print start time for a generation
         next_gen=next_generation(prev_gen)
         compute_fitness(next_gen)
         print("best individual's fitness in "+str(i)+ "th generation : " + str(best_individual(next_gen).fitness))
@@ -18,6 +22,11 @@ def main():
         print("Average generation's fitness "+str(average_fitness(next_gen)))
         average_file.write(str(average_fitness(next_gen))+"\n")
         prev_gen=next_gen
+        end_time=time.time()
+        time_interval=end_time-start_time
+        print(time_interval)
+        time_file.write(str(time_interval)+"\n")
+        # print end time for generation and compute the time taken by that generation
     average_file.close()
     best_file.close()
     features_file.close()
